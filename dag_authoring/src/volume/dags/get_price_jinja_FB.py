8af128b3fdd387e384d892660d1fe2aad00fceb8
Kattson Bastos <kattsonbastos@gmail.com>
2023-04-14 00:03:57 +0000

    feat: multiple dags with jinja and yaml

---
+++ dag_authoring/src/volume/dags/get_price_jinja_FB.py
@@ -0,0 +1,25 @@
+from airflow import DAG
+from airflow.decorators import task
+
+from datetime import datetime
+
+with DAG("get_price_jinja_FB", start_date=datetime(2022,1, 1), 
+        schedule_interval="@daily", 
+        catchup=False,
+        tags = ["dynamic_jinja"]
+    ) as dag:
+        
+    @task
+    def extract(symbol):
+        return symbol
+        
+    @task
+    def process(symbol):
+        return symbol
+        
+    @task
+    def send_email(symbol):
+        print(symbol)
+        return symbol
+        
+    send_email(process(extract(198)))
